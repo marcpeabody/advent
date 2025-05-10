c85909874efece8d35483591ac312cf0018477fc
Marc Peabody <marcpeabody@yahoo.com>
2025-05-10 19:17:48 -0400

    Day 2 Part 1
    Co-authored-by: Rodrigo Otsuka <akio2631@gmail.com>

--- advent.exs
+++ advent.exs
@@ -19,6 +19,46 @@ defmodule Advent do
       acc + number * freq * Map.get(counts_two, number, 0)
     end)
   end
+
+  def day_02_part_1() do
+    InputLists.reports()
+    |> Enum.reduce(0, fn report, acc ->
+      acc + if safe?(report), do: 1, else: 0
+    end)
+  end
+
+  def day_02_part_2() do
+    # InputLists.reports()
+    "test"
+  end
+
+  def safe?([one, one | _tail]), do: false
+
+  def safe?(list = [one, two | _tail]) do
+    if one > two do
+      safe?(list, :desc)
+    else
+      safe?(list, :asc)
+    end
+  end
+
+  def safe?([one, two | tail], :asc) do
+    cond do
+      two - one > 3 -> false
+      one >= two -> false
+      true -> safe?([two | tail], :asc)
+    end
+  end
+
+  def safe?([one, two | tail], :desc) do
+    cond do
+      one - two > 3 -> false
+      two >= one -> false
+      true -> safe?([two | tail], :desc)
+    end
+  end
+
+  def safe?([_], _direction), do: true
 end
 
 _ = Code.require_file("input_lists.ex")
@@ -26,3 +66,5 @@ _ = Code.require_file("input_lists.ex")
 
 IO.inspect("Day 01 part 1: #{Advent.day_01_part_1(list_one, list_two)}")
 IO.inspect("Day 01 part 2: #{Advent.day_01_part_2(list_one, list_two)}")
+IO.inspect("Day 02 part 1: #{Advent.day_02_part_1()}")
+IO.inspect("Day 02 part 2: #{Advent.day_02_part_2()}")
--- input_lists.ex
+++ input_lists.ex
@@ -1,6 +1,6 @@
 defmodule InputLists do
   def lists do
-    File.read!("input.txt")
+    File.read!("day01_input.txt")
     |> String.split("\n")
     |> Enum.reduce({[], []}, fn row, {list_one, list_two} ->
       [left, right] = String.split(row, "   ")
@@ -9,6 +9,16 @@ defmodule InputLists do
         [String.to_integer(left) | list_one],
         [String.to_integer(right) | list_two]
       }
+    end)
+  end
+
+  def reports do
+    File.read!("day02_input.txt")
+    |> String.split("\n")
+    |> Enum.map(fn report ->
+      report
+      |> String.split(" ")
+      |> Enum.map(&String.to_integer/1)
     end)
   end
 end
